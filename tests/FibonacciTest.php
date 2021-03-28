bf66caeca503afd2c83606f68af2da48b96174b9
Riccardo Fiorenza <ricfio.professional@gmail.com>
2021-03-28 20:02:36 +0200

    test: add testFibonacciSequenceIsCorrect

--- tests/FibonacciTest.php
+++ tests/FibonacciTest.php
@@ -11,28 +11,53 @@ use PHPUnit\Framework\TestCase;
  */
 final class FibonacciTest extends TestCase
 {
-    private static $fibonacci;
+    private $fibonacci;
 
-    public static function setUpBeforeClass(): void
+    protected function setUp(): void
     {
-        self::$fibonacci = new Fibonacci();
+        $this->fibonacci = new Fibonacci();
     }
 
     public function testThe1stFibonacciNumberIsZero()
     {
-        $actual = self::$fibonacci->next();
+        $actual = $this->fibonacci->next();
         $this->assertSame(0, $actual);
     }
 
     public function testThe2ndFibonacciNumberIsOne()
     {
-        $actual = self::$fibonacci->next();
+        $actual = $this->fibonacci->next();
+        $actual = $this->fibonacci->next();
         $this->assertSame(1, $actual);
     }
 
     public function testThe3rdFibonacciNumberIsOne()
     {
-        $actual = self::$fibonacci->next();
+        $actual = $this->fibonacci->next();
+        $actual = $this->fibonacci->next();
+        $actual = $this->fibonacci->next();
         $this->assertSame(1, $actual);
+    }
+
+    /**
+     * @dataProvider fibonacciSequence
+     */
+    public function testFibonacciSequenceIsCorrect(int $key, int $expected)
+    {
+        for ($i=0; $i <= $key; $i++) { 
+            $actual = $this->fibonacci->next();
+        }
+        $this->assertSame($expected, $actual);
+    }
+
+    public function fibonacciSequence(): array
+    {
+        $data = [];
+        $list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+        foreach ($list as $key => $number) {
+            $data[] = [$key, $number];
+        }
+
+        return $data;
     }
 }
