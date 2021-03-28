2fb2062dab976d2544383a6f9c2ca58a03b2b1b5
Riccardo Fiorenza <ricfio.professional@gmail.com>
2021-03-28 21:03:41 +0200

    test: add testFibonacci

---
+++ tests/FibonacciTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Fibonacci;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+final class FibonacciTest extends TestCase
+{
+    public function testFibonacci()
+    {
+        $this->assertEquals(0, Fibonacci::f(0));
+    }
+}
