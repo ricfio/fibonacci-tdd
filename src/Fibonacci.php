c9a08f6a9df3089a5ddb6594e6eb747e51cd9eae
Riccardo Fiorenza <ricfio.professional@gmail.com>
2021-03-28 20:30:27 +0200

    test: fix testFibonacciSequenceIsCorrect

--- src/Fibonacci.php
+++ src/Fibonacci.php
@@ -9,15 +9,22 @@ final class Fibonacci
     /**
      * Current index in the Fibonacci sequence.
      */
-    private int $_current = 0;
+    private int $_i = 0;
 
     public function next(): int
     {
-        $_current = $this->_current++;
-        if (0 == $_current) {
-            return 0;
-        }
+        $i = $this->_i++;
+        return $this->_f($i);
+    }
 
-        return 1;
+    private function _f(int $i): int
+    {
+        if (0 == $i) {
+            return 0;
+        } else if (1 == $i) {
+            return 1;
+        } else {
+            return $this->_f($i - 2) + $this->_f($i - 1);
+        }
     }
 }
